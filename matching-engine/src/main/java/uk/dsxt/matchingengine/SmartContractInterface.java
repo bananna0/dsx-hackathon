439788e8f5db8c8aa3208e00f03ac5f6206f7322
Dmitry Zubarevich <dz@dsx.uk>
2019-02-09 17:33:19 +0300

    add number to deal

--- matching-engine/src/main/java/uk/dsxt/matchingengine/SmartContractInterface.java
+++ matching-engine/src/main/java/uk/dsxt/matchingengine/SmartContractInterface.java
@@ -2,7 +2,7 @@ package uk.dsxt.matchingengine;
 
 public interface SmartContractInterface {
 
-    void dealCreated(String currencyPair, long amount, long price,
+    void dealCreated(long number, String currencyPair, long amount, long price,
                      long buyerAmount, long buyerClientOrderId, String buyerAddress, String buyerSign,
                      long sellerAmount, long sellerClientOrderId, String sellerAddress, String sellerSign,
                      String exchangeSing);
