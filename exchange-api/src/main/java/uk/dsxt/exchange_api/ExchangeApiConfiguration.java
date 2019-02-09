5569e9874a9a660518f042e31e6b2be56406a4d1
Dmitry Zubarevich <dz@dsx.uk>
2019-02-09 17:29:28 +0300

    move implementation from mock to real exchange

--- exchange-api/src/main/java/uk/dsxt/exchange_api/ExchangeApiConfiguration.java
+++ exchange-api/src/main/java/uk/dsxt/exchange_api/ExchangeApiConfiguration.java
@@ -2,12 +2,13 @@ package uk.dsxt.exchange_api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uk.dsxt.matchingengine.Exchange;
 import uk.dsxt.matchingengine.ExchangeInterface;
 
 @Configuration
 public class ExchangeApiConfiguration {
     @Bean
     public ExchangeInterface exchangeInterface() {
-        return new MockExchange();
+        return new Exchange();
     }
 }
