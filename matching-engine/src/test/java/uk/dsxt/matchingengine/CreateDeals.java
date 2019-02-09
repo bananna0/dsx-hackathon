622a08184e04d30cbbac35ab8b3c57366fbbc374
Kramskoi Sergei <sk@dsx.uk>
2019-02-09 18:49:48 +0300

    - add deal test

--- matching-engine/src/test/java/uk/dsxt/matchingengine/CreateDeals.java
+++ matching-engine/src/test/java/uk/dsxt/matchingengine/CreateDeals.java
@@ -4,13 +4,26 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import uk.dsxt.matchingengine.datamodel.Trade;
 
+import java.util.ArrayList;
+
 public class CreateDeals extends BaseTest {
 
     @Test
     public void twoOrdersMatchingCorrectAmount() {
         testExchange.openOrder(eurusd, 1, 1, OrderDirection.SELL, 1, addressClient1, signClient1);
         testExchange.openOrder(eurusd, 1, 1, OrderDirection.BUY, 2, addressClient2, signClient2);
-        Trade trade = smartContractInterfaceMock.trades.get(0);
-        Assert.assertEquals(trade.getAmount(), 1);
+        ArrayList<Trade> trades = smartContractInterfaceMock.trades;
+        Assert.assertEquals(trades.get(0).getAmount(), 1);
+    }
+
+    @Test
+    public void moreThanOneDeal() {
+        testExchange.openOrder(eurusd, 1, 1, OrderDirection.SELL, 1, addressClient1, signClient1);
+        testExchange.openOrder(eurusd, 1, 1, OrderDirection.SELL, 2, addressClient1, signClient1);
+        testExchange.openOrder(eurusd, 2, 1, OrderDirection.BUY, 3, addressClient2, signClient2);
+        ArrayList<Trade> trades = smartContractInterfaceMock.trades;
+        Assert.assertEquals(trades.size(), 2);
+        Assert.assertEquals(trades.get(0).getAmount(), 1);
+        Assert.assertEquals(trades.get(1).getAmount(), 1);
     }
 }
--- matching-engine/src/test/java/uk/dsxt/matchingengine/SmartContractInterfaceMockImpl.java
+++ matching-engine/src/test/java/uk/dsxt/matchingengine/SmartContractInterfaceMockImpl.java
@@ -3,10 +3,9 @@ package uk.dsxt.matchingengine;
 import uk.dsxt.matchingengine.datamodel.Trade;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class SmartContractInterfaceMockImpl implements SmartContractInterface {
-    public List<Trade> trades = new ArrayList<>();
+    public ArrayList<Trade> trades = new ArrayList<>();
 
     @Override
     public void dealCreated(long number, String currencyPair, long amount, long price, long buyerAmount, long buyerClientOrderId, String buyerAddress, String buyerSign, long sellerAmount, long sellerClientOrderId, String sellerAddress, String sellerSign, String exchangeSing) {
