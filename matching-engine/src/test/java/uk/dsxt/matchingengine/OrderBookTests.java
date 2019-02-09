0c167d7dee44f60ec12820cbda2c0aa3d1ef9b94
Kramskoi Sergei <sk@dsx.uk>
2019-02-09 19:38:54 +0300

    - add orderbook tests more

--- matching-engine/src/test/java/uk/dsxt/matchingengine/OrderBookTests.java
+++ matching-engine/src/test/java/uk/dsxt/matchingengine/OrderBookTests.java
@@ -7,13 +7,45 @@ import uk.dsxt.matchingengine.datamodel.OrderBook;
 public class OrderBookTests extends BaseTest {
 
     @Test
+    public void checkBuyOneOrder() {
+        testExchange.openOrder(eurusd, 4, 3, OrderDirection.SELL, 1, addressClient1, signClient1);
+        OrderBook orderBook = testExchange.getOrderbook(eurusd);
+        Assert.assertEquals(orderBook.getSellSide().length, 1);
+        Assert.assertEquals(orderBook.getSellSide()[0].getPrice(), 3);
+        Assert.assertEquals(orderBook.getSellSide()[0].getVolume(), 4);
+        Assert.assertEquals(orderBook.getBuySide().length, 0);
+    }
+    /**
+     * Make order and look that orderbook has been created
+     */
+    @Test
     public void makeOSimpleOrderBook() {
         testExchange.openOrder(eurusd, 4, 3, OrderDirection.SELL, 1, addressClient1, signClient1);
         testExchange.openOrder(eurusd, 2, 1, OrderDirection.BUY, 1, addressClient1, signClient1);
         OrderBook orderBook = testExchange.getOrderbook(eurusd);
+        Assert.assertEquals(orderBook.getSellSide().length, 1);
         Assert.assertEquals(orderBook.getSellSide()[0].getPrice(), 3);
         Assert.assertEquals(orderBook.getSellSide()[0].getVolume(), 4);
+        Assert.assertEquals(orderBook.getBuySide().length, 1);
         Assert.assertEquals(orderBook.getBuySide()[0].getPrice(), 1);
         Assert.assertEquals(orderBook.getBuySide()[0].getVolume(), 2);
+    }
+
+    /**
+     * create limit 2 limit orders on the one band (row), and look that sum volume is correct for each side
+     */
+    @Test
+    public void moreThenOneOrdersAtOneBand() {
+        testExchange.openOrder(eurusd, 4, 3, OrderDirection.SELL, 1, addressClient1, signClient1);
+        testExchange.openOrder(eurusd, 3, 3, OrderDirection.SELL, 1, addressClient1, signClient1);
+        testExchange.openOrder(eurusd, 2, 1, OrderDirection.BUY, 1, addressClient1, signClient1);
+        testExchange.openOrder(eurusd, 2, 1, OrderDirection.BUY, 1, addressClient1, signClient1);
+        OrderBook orderBook = testExchange.getOrderbook(eurusd);
+        Assert.assertEquals(orderBook.getSellSide().length, 1);
+        Assert.assertEquals(orderBook.getSellSide()[0].getPrice(), 3);
+        Assert.assertEquals(orderBook.getSellSide()[0].getVolume(), 7);
+        Assert.assertEquals(orderBook.getBuySide().length, 1);
+        Assert.assertEquals(orderBook.getBuySide()[0].getPrice(), 1);
+        Assert.assertEquals(orderBook.getBuySide()[0].getVolume(), 4);
     }
 }
