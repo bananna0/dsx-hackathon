c28273c1f6efa8925ee356f2db7e62e78a08b1a7
george.mavchun <gm@dsx.uk>
2019-02-09 18:35:15 +0300

    Matching engine - first working prototype

--- matching-engine/src/main/java/uk/dsxt/matchingengine/Launcher.java
+++ matching-engine/src/main/java/uk/dsxt/matchingengine/Launcher.java
@@ -1,11 +1,19 @@
 package uk.dsxt.matchingengine;
 
+import lombok.extern.log4j.Log4j2;
+import uk.dsxt.matchingengine.datamodel.OpenOrderResultInternal;
+
+@Log4j2
 public class Launcher {
 
     public static void main(String[] args) {
         MatchingEngine engine = new MatchingEngine("EURUSD");
 
-        engine.openOrder("EURUSD", 777, 10, OrderDirection.BUY, 1, "address", "sign");
+        OpenOrderResultInternal result1 = engine.openOrder("EURUSD", 777, 10, OrderDirection.BUY, 1, "address", "sign");
+        log.debug(result1);
+        OpenOrderResultInternal result2 = engine.openOrder("EURUSD", 777, 10, OrderDirection.SELL, 1, "address", "sign");
+        log.debug(result2);
+
     }
 
 }
--- matching-engine/src/main/java/uk/dsxt/matchingengine/MatchingEngine.java
+++ matching-engine/src/main/java/uk/dsxt/matchingengine/MatchingEngine.java
@@ -46,69 +46,73 @@ public class MatchingEngine {
             return new OpenOrderResultInternal(orderToOpen.getNumber(), OpenOrderResultCode.FAILED, new ArrayList<>());
         }
 
+        List<Trade> trades = new ArrayList<>();
+
         switch (orderToOpen.getDirection()) {
             case BUY: {
-
-                List<Trade> trades = new ArrayList<>();
-
-                for (Iterator<PriceLevel> levelIterator = sellSide.iterator(); levelIterator.hasNext(); ) {
-                    PriceLevel level = levelIterator.next();
-
-                    if (orderToOpen.getPrice() < level.getPrice()) {
-                        break;
-                    }
-
-                    for (Iterator<Order> orderIterator = level.getOrders().iterator(); orderIterator.hasNext(); ) {
-                        Order order = orderIterator.next();
-
-                        long minAmount = Long.min(orderToOpen.getAmount(), order.getAmount());
-
-                        long orderAmountUpdated = order.getAmount() - minAmount;
-                        order.setAmount(orderAmountUpdated);
-
-                        long orderToOpenAmountUpdated = orderToOpen.getAmount() - minAmount;
-                        orderToOpen.setAmount(orderToOpenAmountUpdated);
-
-                        Trade trade = new Trade(tradeNumber++, minAmount, level.getPrice());
-                        log.debug("Trade created: {}" + trade);
-                        trades.add(trade);
-
-                        if (orderAmountUpdated == 0) {
-                            orderIterator.remove();
-                        }
-
-                        if (orderToOpenAmountUpdated == 0) {
-                            break;
-                        }
-                    }
-
-                    if (level.getOrders().isEmpty()) {
-                        levelIterator.remove();
-                    }
-                }
-
-                this.trades.addAll(trades);
-
-                if (orderToOpen.getAmount() > 0) {
-                    if (!buySide.isEmpty() && buySide.peek().getPrice() >= orderToOpen.getPrice()) {
-                        log.error("FATAL EXCEPTION - SERVER STOPPED");
-                        return new OpenOrderResultInternal(orderToOpen.getNumber(), OpenOrderResultCode.FATAL_ERROR, new ArrayList<>());
-                    }
-                    buySide.add(new PriceLevel(orderToOpen));
-                }
-
-                return new OpenOrderResultInternal(orderToOpen.getNumber(), OpenOrderResultCode.SUCCESS, trades);
+                orderbookMatching(sellSide, buySide, orderToOpen, trades, 1);
+                break;
             }
-
             case SELL: {
-
-                // TODO Implement
-
+                orderbookMatching(buySide, sellSide, orderToOpen, trades, -1);
+                break;
             }
         }
 
-        // TODO Remove it
-        return new OpenOrderResultInternal(orderToOpen.getNumber(), OpenOrderResultCode.FAILED, new ArrayList<>());
+        this.trades.addAll(trades);
+
+        return new OpenOrderResultInternal(orderToOpen.getNumber(), OpenOrderResultCode.SUCCESS, trades);
+    }
+
+    private void orderbookMatching(Queue<PriceLevel> side, Queue<PriceLevel> otherSide, Order orderToOpen, List<Trade> trades, int AI) {
+        for (Iterator<PriceLevel> levelIterator = side.iterator(); levelIterator.hasNext(); ) {
+            PriceLevel level = levelIterator.next();
+
+            if ((orderToOpen.getPrice() - level.getPrice()) * AI < 0) {
+                break;
+            }
+
+            levelMatching(orderToOpen, trades, level);
+
+            if (level.getOrders().isEmpty()) {
+                levelIterator.remove();
+            }
+        }
+
+        if (orderToOpen.getAmount() > 0) {
+            if (!otherSide.isEmpty() && (otherSide.peek().getPrice() - orderToOpen.getPrice()) * AI >= 0) {
+                log.error("FATAL EXCEPTION - SERVER STOPPED");
+                System.exit(0);
+            }
+            otherSide.add(new PriceLevel(orderToOpen));
+        }
+    }
+
+    private void levelMatching(Order orderToOpen, List<Trade> trades, PriceLevel level) {
+        for (Iterator<Order> orderIterator = level.getOrders().iterator(); orderIterator.hasNext(); ) {
+            Order order = orderIterator.next();
+
+            long minAmount = Long.min(orderToOpen.getAmount(), order.getAmount());
+            log.debug("minAmount={}", minAmount);
+
+            long orderAmountUpdated = order.getAmount() - minAmount;
+            order.setAmount(orderAmountUpdated);
+
+            long orderToOpenAmountUpdated = orderToOpen.getAmount() - minAmount;
+            orderToOpen.setAmount(orderToOpenAmountUpdated);
+
+            Trade trade = new Trade(tradeNumber++, minAmount, level.getPrice());
+            log.debug("Trade created: {}", trade);
+            trades.add(trade);
+
+            if (orderAmountUpdated == 0) {
+                orderIterator.remove();
+            }
+
+            if (orderToOpenAmountUpdated == 0) {
+                break;
+            }
+        }
     }
 
 }
