672dcb87ec3c4de87714fda9d7bf980eec8a963e
thony <antonprimako@gmail.com>
2019-02-09 20:00:32 +0300

    all tests are passed

--- matching-engine/src/test/java/uk/dsxt/matchingengine/BaseTest.java
+++ matching-engine/src/test/java/uk/dsxt/matchingengine/BaseTest.java
@@ -1,5 +1,9 @@
 package uk.dsxt.matchingengine;
 
+import org.awaitility.Awaitility;
+
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
     public static final String eurusd = "EURUSD";
     public static final String addressClient1 = "address1";
@@ -8,4 +12,11 @@ public class BaseTest {
     public static final String signClient2 = "sign2";
     public SmartContractInterfaceMockImpl smartContractInterfaceMock = new SmartContractInterfaceMockImpl();
     public Exchange testExchange = new Exchange(smartContractInterfaceMock);
+
+    public void waitForTradesCount(int expectedCount, int timeout, int pollInterval) {
+        Awaitility.await("Looking for a " + expectedCount + " trades").timeout(timeout, TimeUnit.SECONDS)
+                .pollInterval(pollInterval, TimeUnit.MILLISECONDS)
+                .pollDelay(0, TimeUnit.SECONDS)
+                .until(() -> smartContractInterfaceMock.trades.size() == expectedCount);
+    }
 }
--- matching-engine/src/test/java/uk/dsxt/matchingengine/CreateDeals.java
+++ matching-engine/src/test/java/uk/dsxt/matchingengine/CreateDeals.java
@@ -1,6 +1,7 @@
 package uk.dsxt.matchingengine;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import uk.dsxt.matchingengine.datamodel.Trade;
 
@@ -8,10 +9,17 @@ import java.util.ArrayList;
 
 public class CreateDeals extends BaseTest {
 
+    @BeforeMethod
+    public void init(){
+        smartContractInterfaceMock = new SmartContractInterfaceMockImpl();
+        testExchange = new Exchange(smartContractInterfaceMock);
+    }
+
     @Test
     public void twoOrdersMatchingCorrectAmount() {
         testExchange.openOrder(eurusd, 1, 1, OrderDirection.SELL, 1, addressClient1, signClient1);
         testExchange.openOrder(eurusd, 1, 1, OrderDirection.BUY, 2, addressClient2, signClient2);
+        waitForTradesCount(1, 1, 100);
         ArrayList<Trade> trades = smartContractInterfaceMock.trades;
         Assert.assertEquals(trades.get(0).getAmount(), 1);
     }
@@ -20,6 +28,7 @@ public class CreateDeals extends BaseTest {
     public void twoOrdersMatchingCorrectPrice() {
         testExchange.openOrder(eurusd, 1, 2, OrderDirection.SELL, 1, addressClient1, signClient1);
         testExchange.openOrder(eurusd, 1, 3, OrderDirection.BUY, 2, addressClient2, signClient2);
+        waitForTradesCount(1, 1, 100);
         ArrayList<Trade> trades = smartContractInterfaceMock.trades;
         Assert.assertEquals(trades.get(0).getPrice(), 2);
     }
@@ -29,6 +38,7 @@ public class CreateDeals extends BaseTest {
         testExchange.openOrder(eurusd, 1, 1, OrderDirection.SELL, 1, addressClient1, signClient1);
         testExchange.openOrder(eurusd, 1, 1, OrderDirection.SELL, 2, addressClient1, signClient1);
         testExchange.openOrder(eurusd, 2, 1, OrderDirection.BUY, 3, addressClient2, signClient2);
+        waitForTradesCount(2, 1, 100);
         ArrayList<Trade> trades = smartContractInterfaceMock.trades;
         Assert.assertEquals(trades.size(), 2);
         Assert.assertEquals(trades.get(0).getAmount(), 1);
