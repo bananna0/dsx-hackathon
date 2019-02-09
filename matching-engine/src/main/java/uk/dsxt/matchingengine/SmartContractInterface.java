a51d580780b8dae39f7994cfff58b43139f9cfa2
Dmitry Zubarevich <dz@dsx.uk>
2019-02-09 17:08:29 +0300

    smart contract interface (deals handling)

---
+++ matching-engine/src/main/java/uk/dsxt/matchingengine/SmartContractInterface.java
@@ -0,0 +1,10 @@
+package uk.dsxt.matchingengine;
+
+public interface SmartContractInterface {
+
+    void dealCreated(String currencyPair, long amount, long price,
+                     long buyerAmount, long buyerClientOrderId, String buyerAddress, String buyerSign,
+                     long sellerAmount, long sellerClientOrderId, String sellerAddress, String sellerSign,
+                     String exchangeSing);
+
+}
