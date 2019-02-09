dc9b0bc76347b5ca4f76ef7174b0252c305aadc8
Kramskoi Sergei <sk@dsx.uk>
2019-02-09 17:05:28 +0300

    - use real Exchange implementation

--- matching-engine/src/test/java/uk/dsxt/matchingengine/BaseTest.java
+++ matching-engine/src/test/java/uk/dsxt/matchingengine/BaseTest.java
@@ -6,18 +6,5 @@ public class BaseTest {
     public static final String addressClient2 = "address2";
     public static final String signClient1 = "sign1";
     public static final String signClient2 = "sign2";
-    public TestExchange testExchange = new TestExchange();
-
-    class TestExchange implements ExchangeInterface {
-
-        @Override
-        public OpenOrderResult openOrder(String currencyPair, long amount, long price, OrderDirection direction, long clientOrderId, String address, String sign) {
-            return null;
-        }
-
-        @Override
-        public boolean cancelOrder(long clientOrderId) {
-            return false;
-        }
-    }
+    public Exchange testExchange = new Exchange();
 }
